d506b46bf5432773e7f95b48cc981253e075ef2e
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-14 21:38:09 +0300

    functionClass

---
+++ functionClass.js
@@ -0,0 +1,21 @@
+//! Regular Ball Super Ball
+
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+var Ball = function (ballType) {
+  this.ballType = ballType || "regular";
+};
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+console.log(ball1.ballType);
+console.log(ball2.ballType);
