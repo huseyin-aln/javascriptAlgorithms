74588fd8a9073ad5965c53954005689b4fee6b5f
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-21 23:04:04 +0300

    pascalCase v2

--- pascalCaseConverter.js
+++ pascalCaseConverter.js
@@ -1,3 +1,7 @@
+//! Convert PascalCase string into snake_case
+
+// Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
+
 function pascalCaseConverter(string) {
   return (
     string.slice(0, 1).toLowerCase() +
