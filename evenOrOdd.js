28bb15ca805176870d25dd83415411cc7e8ee6a2
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2023-01-16 22:56:44 +0300

    evenOrOdd

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+//! Even or Odd
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+  return number % 2 ? "Odd" : "Even";
+}
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(1));
