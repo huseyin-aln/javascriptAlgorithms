5a8e41aa0670978ef1aded84d704f8de70b4fc58
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2023-03-03 22:55:38 +0300

    greetMe

---
+++ greetMe.js
@@ -0,0 +1,19 @@
+//! Greet Me
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+const greet = (name) => {
+  let capitalizedName =
+    name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+
+  return `Hello ${capitalizedName}!`;
+};
+
+// const greet = name => "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+
+console.log(greet("riley")); // Hello Riley!
